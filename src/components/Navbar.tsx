fa86ba5e7d02990586211bf613c809642b4dde20
Neesh <avaneeshsamsi@gmail.com>
2023-05-06 22:56:48 +0530

    feat: Add hover animation to Logo link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,10 @@ const Navbar = ({ pathname }: { pathname: string }) => {
   return (
     <nav className="sticky top-0 z-10 bg-light py-8 shadow-lg shadow-stone-900/5">
       <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-between gap-8 px-8 sm:flex-row sm:gap-0">
-        <Link href="/" className="text-xl font-bold text-accent ">
+        <Link
+          href="/"
+          className="text-xl font-bold text-accent transition-opacity hover:opacity-70"
+        >
           Darshan
         </Link>
         <div className="flex gap-6 text-lg">
