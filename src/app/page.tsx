a7f183e9aed89f4c042ded05f108a2c61a45ba46
Neesh <avaneeshsamsi@gmail.com>
2023-05-07 16:57:14 +0530

    feat: Dynamic blog cards on home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import BlogCard from "@/components/BlogCard"
+import { getPosts } from "@/posts"
 import Image from "next/image"
 import Link from "next/link"
 
 const verticalBlogGap = "gap-16 xl:gap-6"
 
 export default function Home() {
+  const posts = getPosts().slice(0, 4)
+
+  const featuredPost = posts.shift()
+
   return (
     <>
       <main className="mx-auto flex max-w-screen-xl flex-col items-start gap-8 px-8 py-16 md:flex-row md:items-center md:gap-24">
@@ -28,52 +33,39 @@ export default function Home() {
       </main>
       <section id="blog" className="mx-auto max-w-screen-xl px-8 py-16">
         <h2 className="text-5xl font-bold text-accent">My Blog</h2>
-        <div
-          className={`flex max-w-2xl flex-col py-16 xl:w-full xl:max-w-none xl:flex-row ${verticalBlogGap}`}
-        >
-          <div className="xl:w-1/2">
-            <BlogCard
-              type="Regular"
-              img="https://cdn.tuk.dev/assets/components/111220/blg-17/blog1.png"
-              alt="Blog card image"
-              date="12th April, 2023"
-              title="Beautiful Italy, Travel Blog"
-              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries. Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-              slug="one"
-            />
+        {featuredPost && posts ? (
+          <div
+            className={`flex max-w-2xl flex-col py-16 xl:w-full xl:max-w-none xl:flex-row ${verticalBlogGap}`}
+          >
+            <div className="xl:w-1/2">
+              <BlogCard
+                type="Regular"
+                img={featuredPost.imgUrl}
+                alt={featuredPost.imgAlt}
+                date={featuredPost.date}
+                title={featuredPost.title}
+                description={featuredPost.description}
+                slug={featuredPost.id}
+              />
+            </div>
+            <div className={`flex flex-col xl:w-1/2 ${verticalBlogGap}`}>
+              {posts.map((post) => (
+                <BlogCard
+                  key={post.id}
+                  type="Sidebar"
+                  img={post.imgUrl}
+                  alt={post.imgAlt}
+                  date={post.date}
+                  title={post.title}
+                  description={post.description}
+                  slug={post.id}
+                />
+              ))}
+            </div>
           </div>
-          <div className={`flex flex-col xl:w-1/2 ${verticalBlogGap}`}>
-            <BlogCard
-              type="Sidebar"
-              img="https://cdn.tuk.dev/assets/components/111220/blg-17/blog2.png"
-              alt="Blog card image"
-              date="13th April, 2023"
-              title="A Broken Backpack"
-              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequuntur illo, ipsa quasi voluptatem totam minus iusto cumque sunt provident."
-              slug="two"
-            />
-
-            <BlogCard
-              type="Sidebar"
-              img="https://cdn.tuk.dev/assets/components/111220/blg-17/blog3.png"
-              alt="Blog card image"
-              date="14th April, 2023"
-              title="My life’s a Movie"
-              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries. Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-              slug="three"
-            />
-
-            <BlogCard
-              type="Sidebar"
-              img="https://cdn.tuk.dev/assets/components/111220/blg-17/blog4.png"
-              alt="Blog card image"
-              date="15th April, 2023"
-              title="Lilii’s Travel Plans"
-              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries. Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-              slug="three"
-            />
-          </div>
-        </div>
+        ) : (
+          <p>No latest posts.</p>
+        )}
 
         <Link href="/blog">
           <button className="block rounded border-2 border-accent px-6 py-3 font-semibold text-accent transition-all hover:bg-accent hover:text-light focus:outline-none md:text-lg xl:mx-auto xl:px-8 xl:py-4 xl:text-2xl">
