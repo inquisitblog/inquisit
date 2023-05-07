b9720c04889301743c2cd6ac3240a07e2910dffd
Neesh <avaneeshsamsi@gmail.com>
2023-05-07 17:22:21 +0530

    feat: Add function to get single post

--- src/posts.tsx
+++ src/posts.tsx
@@ -1,6 +1,8 @@
 import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
+import { remark } from "remark"
+import html from "remark-html"
 
 const postsDirectory = path.join(process.cwd(), "blogposts")
 
@@ -37,4 +39,31 @@ export function getPosts(limit?: number): BlogPost[] {
   const allPosts = limit ? sortedPosts.slice(0, limit) : sortedPosts
 
   return sortedPosts
+}
+
+export async function getPost(id: string) {
+  const fullPath = path.join(postsDirectory, id + ".md")
+  const fileContents = fs.readFileSync(fullPath, "utf8")
+
+  // Use matter to parse metadata
+  const matterResult = matter(fileContents)
+
+  // Process md into html
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  // Meta + html content + id
+  const blogPostWithHtml: BlogPost & { contentHtml: string } = {
+    id,
+    title: matterResult.data.title,
+    description: matterResult.data.description,
+    imgUrl: matterResult.data.imgUrl,
+    imgAlt: matterResult.data.imgAlt,
+    date: matterResult.data.date,
+    contentHtml,
+  }
+
+  return blogPostWithHtml
 }
