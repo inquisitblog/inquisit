87a864cd348af08cbda9962bb8fb70b1a89262cd
Neesh <avaneeshsamsi@gmail.com>
2023-05-08 22:21:25 +0530

    idk why I removed it in the first place

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["placebeard.it", "imgur.com", "i.imgur.com"],
+    domains: ["placebeard.it", "imgur.com", "i.imgur.com", "cdn.tuk.dev"],
   },
   experimental: {
     appDir: true,
