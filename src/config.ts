a98c62ebfb13b047b86208e95ad473dfe79d5245
Neesh <avaneeshsamsi@gmail.com>
2023-05-08 15:17:46 +0530

    feat: Add page meta to config

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,19 @@ process && process.env.NODE_ENV === "development"
 
 export const dev = inDevEnv
 
-export const title = "Darshan"
-export const description = "Description"
+// IGNORE ABOVE THIS
+
+// General Settings
 export const url = !dev
   ? "https://darshan-blog.vercel.app"
   : "http://localhost:3000"
 export const author = "Darshan"
 export const twitterUsername = ""
+
+// Home Page
+export const title = "Darshan"
+export const description = "Description"
+
+// Blog Page
+export const blogTitle = "Blog" // | {title} is added to it => Blog | Darshan
+export const blogDescription = "Check out my latest articles."
