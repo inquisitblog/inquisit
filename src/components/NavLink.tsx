fccfcdb8e365fb8a1148b4f6f677bcfa34980254
Neesh <avaneeshsamsi@gmail.com>
2023-05-08 17:39:01 +0530

    feat: Add manually add active prop for underline

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -4,14 +4,17 @@ type NavLinkProps = {
   path: string
   text: string
   pathname?: string
+  active?: boolean
 }
 
-const NavLink = ({ text, path, pathname }: NavLinkProps) => {
+const NavLink = ({ text, path, pathname, active }: NavLinkProps) => {
+  const isActive = active ? true : pathname === path ? true : false
+
   return (
     <Link
       href={path}
-      className={`relative w-fit transition-opacity before:absolute before:bottom-0  before:left-0 before:h-[2px] before:w-full before:scale-x-0  before:bg-accent before:transition-transform hover:opacity-70 hover:before:scale-x-100 ${
-        pathname === path ? "before:scale-x-100" : ""
+      className={`relative w-fit transition-opacity before:absolute before:-bottom-1  before:left-0 before:h-[2px] before:w-full before:scale-x-0  before:bg-accent before:transition-transform hover:opacity-70 hover:before:scale-x-100 ${
+        isActive ? "before:scale-x-100" : ""
       }`}
     >
       {text}
