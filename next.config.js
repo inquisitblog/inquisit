8eb5a30d855d20b477c8aba8b1d1b90c82c0af54
Neesh <avaneeshsamsi@gmail.com>
2023-05-09 21:24:10 +0530

    feat: Add convenient redirects - rss & categories

--- next.config.js
+++ next.config.js
@@ -3,9 +3,18 @@ const nextConfig = {
   images: {
     domains: ["placebeard.it", "imgur.com", "i.imgur.com", "cdn.tuk.dev"],
   },
-  experimental: {
-    appDir: true,
-  },
+  redirects: async () => [
+    {
+      source: "/blog/feed",
+      destination: "/rss.xml",
+      permanent: true,
+    },
+    {
+      source: "/categories",
+      destination: "/blog/categories",
+      permanent: true,
+    },
+  ],
 }
 
 module.exports = nextConfig
