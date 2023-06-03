08801f6ec8f46da0315551f0022a622a504104f9
Neesh <avaneeshsamsi@gmail.com>
2023-06-03 17:38:42 +0530

    feat: Add about section boxes to Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import BlogCard from "@/components/BlogCard"
 import { getPosts } from "@/lib/posts"
 import Image from "next/image"
 import Link from "next/link"
+import { FC, PropsWithChildren } from "react"
 
 const verticalBlogGap = "gap-16 xl:gap-6"
 
@@ -17,24 +18,63 @@ export default function Home() {
 
   return (
     <>
-      <main className="mx-auto max-w-screen-xl px-8 py-12 md:py-24">
-        <div className="text-center">
-          <h1 className="text-4xl font-semibold md:text-5xl xl:text-6xl mb-4">
-            Welcome to <span className="text-accent">Inquisist</span>
-          </h1>
-          <p className="text-xl md:text-2xl xl:text-3xl text-gray-700 mb-8">
-            {config.heroTagline}
-          </p>
-          <button
-            className="block rounded border-2 border-accent px-6 py-3 font-semibold text-accent transition-all hover:bg-accent hover:text-light focus:outline-none md:text-lg xl:mx-auto xl:px-8 xl:py-4 xl:text-2xl"
-          >
-            About
-          </button>
+      <main className="mx-auto max-w-screen-xl px-8 py-12 text-center md:py-24">
+        <h1 className="text-4xl font-semibold md:text-5xl xl:text-6xl">
+          Welcome to <span className="text-accent">Inquisit</span>
+        </h1>
+        <p className="mt-4 text-xl text-dark/80 md:mt-8 md:text-2xl xl:text-3xl">
+          {config.heroTagline}
+        </p>
+        <div className="mt-12 grid grid-cols-1 gap-8 md:mt-24 md:gap-8 lg:grid-cols-2 xl:gap-16">
+          <InfoCard title={{ text: "The", accent: "Blog" }}>
+            {config.aboutBlog.map((paragraph, index) => (
+              <p
+                key={index}
+                className="mt-6 text-base text-dark/80 md:text-lg xl:text-xl"
+              >
+                {paragraph}
+              </p>
+            ))}
+          </InfoCard>
+          <InfoCard title={{ text: "The", accent: "People" }}>
+            {config.people.map((person, i) => {
+              const isOdd = i % 2 !== 0
+              return (
+                <div
+                  className={`mt-8 flex flex-col items-center gap-4 text-base text-dark/80 md:text-lg xl:text-xl ${
+                    isOdd ? "md:flex-row-reverse" : "md:flex-row"
+                  }`}
+                  key={i}
+                >
+                  <Image
+                    src={`/${person.avatar}`}
+                    alt={`${person.name}'s image`}
+                    height={200}
+                    width={200}
+                    className="aspect-square w-24 flex-1 rounded-full"
+                  />
+                  <p className={`${isOdd ? "md:text-end" : "md:text-start"}`}>
+                    {person.about}
+                  </p>
+                </div>
+              )
+            })}
+            {/* <div className="mt-4 flex items-center">
+                <p className="text-lg text-dark/80 xl:text-xl">
+                  {config.aboutNeesh}
+                </p>
+                <img
+                  src="https://iili.io/HrDCg0Q.png"
+                  alt="Neesh's image"
+                  className="mr-4 h-40 w-40 rounded-full"
+                />
+              </div> */}
+          </InfoCard>
         </div>
       </main>
 
-      <section id="blog" className="mx-auto max-w-screen-xl px-8 pt-8 pb-16">
-        <h1 className="text-5xl font-bold text-accent">Latest posts</h1>
+      <section id="blog" className="mx-auto max-w-screen-xl px-8 pb-16 pt-8">
+        <h3 className="text-5xl font-bold text-accent">Latest posts</h3>
         {featuredPost && posts ? (
           <div
             className={`flex max-w-2xl flex-col py-16 xl:w-full xl:max-w-none xl:flex-row ${verticalBlogGap}`}
@@ -78,43 +118,19 @@ export default function Home() {
           </button>
         </Link>
       </section>
-
-      <section id="about" className="mx-auto max-w-screen-xl px-8 py-16">
-        <h1 className="text-5xl font-bold text-accent mb-16">About</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16">
-          <div className="bg-light rounded-xl p-4 md:p-8">
-            <h2 className="text-2xl font-semibold leading-7 xl:text-3xl mt-0 md:mt-4">
-              The Blog
-            </h2>
-            {config.aboutBlog.map((paragraph, index) => (
-              <p key={index} className="text-lg xl:text-xl text-gray-700 mt-4">
-                {paragraph}
-              </p>
-            ))}
-          </div>
-          <div className="bg-light rounded-xl p-4 md:p-8">
-            <h2 className="text-2xl font-semibold leading-7 xl:text-3xl mt-0 md:mt-4 mb-8">
-              The People
-            </h2>
-            <div className="flex items-center mt-4 mb-8">
-              <img
-                src="https://iili.io/HrDCUqx.png"
-                alt="Shan's image"
-                className="w-40 h-40 rounded-full mr-4"
-              />
-              <p className="text-lg xl:text-xl text-gray-700">{config.aboutShan}</p>
-            </div>
-            <div className="flex items-center mt-4">
-              <p className="text-lg xl:text-xl text-gray-700">{config.aboutNeesh}</p>
-              <img
-                src="https://iili.io/HrDCg0Q.png"
-                alt="Neesh's image"
-                className="w-40 h-40 rounded-full mr-4"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
     </>
+  )
+}
+
+const InfoCard: FC<
+  PropsWithChildren<{ title: { text: string; accent: string } }>
+> = ({ title, children }) => {
+  return (
+    <div className="rounded-xl bg-light px-4 py-6 lg:px-8 lg:py-10">
+      <h2 className="text-xl font-semibold md:text-2xl xl:text-3xl">
+        {title.text} <span className="text-accent">{title.accent}</span>
+      </h2>
+      {children}
+    </div>
   )
 }
