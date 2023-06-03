1a84b41d0c38daf28b27a95e2bdc3c76c96180d0
shan-shan-dar <105597557+shan-shan-dar@users.noreply.github.com>
2023-06-03 14:49:14 +0530

    changed home-page, added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,16 +18,23 @@ export default function Home() {
   return (
     <>
       <main className="mx-auto max-w-screen-xl px-8 py-12 md:py-24">
-        <p className="mb-1 text-xl md:mb-2 xl:text-2xl">Hi, I'm</p>
-        <h1 className="text-4xl font-semibold md:text-5xl xl:text-6xl">
-          Darshan
-        </h1>
-        <p className="mb:mt-4 mt-2 text-2xl xl:text-3xl">
-          {config.heroTagline}
-        </p>
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold md:text-5xl xl:text-6xl mb-4">
+            Welcome to <span className="text-accent">Inquisist</span>
+          </h1>
+          <p className="text-xl md:text-2xl xl:text-3xl text-gray-700 mb-8">
+            {config.heroTagline}
+          </p>
+          <button
+            className="block rounded border-2 border-accent px-6 py-3 font-semibold text-accent transition-all hover:bg-accent hover:text-light focus:outline-none md:text-lg xl:mx-auto xl:px-8 xl:py-4 xl:text-2xl"
+          >
+            About
+          </button>
+        </div>
       </main>
-      <section id="blog" className="mx-auto max-w-screen-xl px-8 py-16">
-        <h2 className="text-5xl font-bold text-accent">Latest posts</h2>
+
+      <section id="blog" className="mx-auto max-w-screen-xl px-8 pt-8 pb-16">
+        <h1 className="text-5xl font-bold text-accent">Latest posts</h1>
         {featuredPost && posts ? (
           <div
             className={`flex max-w-2xl flex-col py-16 xl:w-full xl:max-w-none xl:flex-row ${verticalBlogGap}`}
@@ -70,6 +77,43 @@ export default function Home() {
             Browse More
           </button>
         </Link>
+      </section>
+
+      <section id="about" className="mx-auto max-w-screen-xl px-8 py-16">
+        <h1 className="text-5xl font-bold text-accent mb-16">About</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16">
+          <div className="bg-light rounded-xl p-4 md:p-8">
+            <h2 className="text-2xl font-semibold leading-7 xl:text-3xl mt-0 md:mt-4">
+              The Blog
+            </h2>
+            {config.aboutBlog.map((paragraph, index) => (
+              <p key={index} className="text-lg xl:text-xl text-gray-700 mt-4">
+                {paragraph}
+              </p>
+            ))}
+          </div>
+          <div className="bg-light rounded-xl p-4 md:p-8">
+            <h2 className="text-2xl font-semibold leading-7 xl:text-3xl mt-0 md:mt-4 mb-8">
+              The People
+            </h2>
+            <div className="flex items-center mt-4 mb-8">
+              <img
+                src="https://iili.io/HrDCUqx.png"
+                alt="Shan's image"
+                className="w-40 h-40 rounded-full mr-4"
+              />
+              <p className="text-lg xl:text-xl text-gray-700">{config.aboutShan}</p>
+            </div>
+            <div className="flex items-center mt-4">
+              <p className="text-lg xl:text-xl text-gray-700">{config.aboutNeesh}</p>
+              <img
+                src="https://iili.io/HrDCg0Q.png"
+                alt="Neesh's image"
+                className="w-40 h-40 rounded-full mr-4"
+              />
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -10,7 +10,7 @@ export const navLinks: NavLink[] = [
   { text: "Home", path: "/" },
   { text: "Blog", path: "/blog" },
   { text: "Categories", path: "/categories" },
-  { text: "Submit Entry", path: "https://r2fma1pv48c.typeform.com/to/wXC7QnZC", newWindow: true},
+  { text: "Submit Post", path: "https://r2fma1pv48c.typeform.com/to/wXC7QnZC", newWindow: true},
   // { text: "Feed", path: "/rss.xml", newWindow: true },
 ]
 
@@ -29,9 +29,17 @@ export const footerTagline = "Some sort of tagline?"
 export const email = "darshan2004s@gmail.com"
 
 // Home Page
-export const title = "Darshan"
-export const description = "Description"
-export const heroTagline = "I'm interested in studying AI through an array of interdiciplinary lenses"
+export const title = "Inquisit"
+export const description = "Inquisit is a project intended to incentivize and share academic adventures across a variety of fields. I hope you find value in this blog as a source of inspiration and a platform for showcasing your own interdisciplinary pursuits."
+export const heroTagline = "A project intended to incentivize and share academic adventures across a variety fields. I hope you find value in this blog as a source of inspiration and a platform for showcasing your own interdisciplinary pursuits."
+
+// About Section
+export const aboutBlog = [
+  "Science and philosophy have been inseparable for most of their history. The Greek pioneers of Western scientific inquiry made no distinction between the two, nor did they separate the range of disciplines such as natural sciences, arts, and literature, that we do today.",
+  "The recent three centuries have seen the reduction of science into restrictive disciplines. Today, even as many fields advance rapidly on their own, a loss of the broader perspective hinders the ability of science to address real problems. The need to recultivate a holistic approach to inquiry is important."
+]
+export const aboutShan = "Hi, I’m Darshan, a gap-year student just having bid farewell to Sahyadri School. I’m interested in studying AI and Cognitive Science through an array of interdisciplinary lenses."
+export const aboutNeesh = "Neesh Neesh Neesh Neesh, consectetur adipiscing elit. Nullam interdum eleifend nibh, eget gravida urna bibendum ac. Fusce id volutpat justo. "
 
 // Blog Page
 export const blogTitle = "Blog" // Shows as "this | {title}" => Blog | Darshan
