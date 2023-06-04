05ea56c4b5b95889117c419047d3ea15fd526c58
Neesh <avaneeshsamsi@gmail.com>
2023-06-04 18:25:24 +0530

    feat: Reduce animation duration

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -24,7 +24,7 @@ const ScrollToTop = () => {
 
   return (
     <button
-      className={`hover ring-none sticky bottom-4 right-4 w-12 self-end rounded-full bg-accent p-2 text-light shadow-md shadow-stone-900/50 outline-none transition-all duration-500 hover:border-2 hover:border-accent hover:bg-lighter hover:text-accent hover:outline-none focus:outline-none md:w-16 md:p-4 ${
+      className={`hover ring-none sticky bottom-4 right-4 w-12 self-end rounded-full bg-accent p-2 text-light shadow-md shadow-stone-900/50 outline-none transition-all duration-300 hover:border-2 hover:border-accent hover:bg-lighter hover:text-accent hover:outline-none focus:outline-none md:w-16 md:p-4 ${
         scrollPos && scrollPos > 0
           ? "translate-y-0 opacity-100"
           : "translate-y-24 opacity-0"
