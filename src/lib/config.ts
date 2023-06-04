d68ed6d50d0cb41116f1bef0de0790317b69af82
Neesh <avaneeshsamsi@gmail.com>
2023-06-04 13:31:37 +0530

    feat: Change comment Darshan to Inquisit

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -34,12 +34,14 @@ export const email = "darshan2004s@gmail.com"
 
 // Home Page
 export const title = "Inquisit"
-export const description = "Inquisit is a project about igniting curiosity and exploring diverse academic adventures. I hope you find value in this blog as a source of inspiration and a platform for showcasing your own interdisciplinary pursuits."
-export const heroTagline = "A project about igniting curiosity and exploring diverse academic adventures. I hope you find value in this blog as a source of inspiration and a platform for showcasing your own interdisciplinary pursuits."
+export const description =
+  "Inquisit is a project about igniting curiosity and exploring diverse academic adventures. I hope you find value in this blog as a source of inspiration and a platform for showcasing your own interdisciplinary pursuits."
+export const heroTagline =
+  "A project about igniting curiosity and exploring diverse academic adventures. I hope you find value in this blog as a source of inspiration and a platform for showcasing your own interdisciplinary pursuits."
 export const aboutBlog = [
   "Science and philosophy have been like two peas in a pod throughout history. Back in the day, those bearded Greek pioneers didn't bother with drawing lines. They were suckers for knowledge from every angle possible.",
   "Fast forward to the past three centuries, and science got chopped up into all these restrictive disciplines. Sure, each field is making strides, but we're missing out on the broader perspective.",
-  "We're here to shake things up and tap into that holistic approach to inquiry. This is where science meets philosophy and everything in between."
+  "We're here to shake things up and tap into that holistic approach to inquiry. This is where science meets philosophy and everything in between.",
 ]
 
 export const people = [
@@ -58,5 +60,5 @@ export const people = [
 ]
 
 // Blog Page
-export const blogTitle = "Blog" // Shows as "this | {title}" => Blog | Darshan
+export const blogTitle = "Blog" // Shows as "this | {title}" => Blog | Inquisit
 export const blogDescription = "Check out the latest articles on Inquisit."
