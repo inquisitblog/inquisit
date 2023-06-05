dd01e0fd17db01aadd8cdaeda093ce6fba677cab
Neesh <avaneeshsamsi@gmail.com>
2023-06-05 17:32:32 +0530

    feat: Sitemap works but no posts shown

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,7 @@
 import { MetadataRoute } from "next"
 import * as config from "@/lib/config"
-import { postsDir } from "@/lib/posts"
+// import { getPostIDs } from "@/lib/posts"
 import { getCategories } from "@/lib/categories"
-import fs from "fs"
-import path from "path"
-import matter from "gray-matter"
-
-function getPostIDs(): { id: string; date: string }[] {
-  const fileNames = fs.readdirSync(postsDir)
-
-  const postsIDs = fileNames.map((fileName) => {
-    const id = fileName.replace(/\.mdx$/, "")
-
-    const fullPath = path.join(postsDir, fileName)
-
-    const fileContents = fs.readFileSync(fullPath, "utf8")
-    const matterResult = matter(fileContents)
-
-    return { id, date: matterResult.data.date }
-  })
-
-  return postsIDs
-}
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const pages = ["", "blog", "categories"]
@@ -30,16 +10,20 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: new Date(),
   }))
 
-  const postUrls = getPostIDs().map((post) => ({
-    url: `${config.url}/posts/${post.id}`,
-    lastModified: new Date(post.date),
-  }))
+  // const postUrls = getPostIDs().map((post) => ({
+  //   url: `${config.url}/blog/${post.id}`,
+  //   lastModified: new Date(post.date),
+  // }))
 
   const categories = getCategories()
   const categoryUrls = categories.map((tag) => ({
-    url: `${config.url}/blog/categories/${tag}`,
+    url: `${config.url}/blog/categories/${tag.slug}`,
     lastModified: new Date(),
   }))
 
-  return [...pageUrls, ...postUrls, ...categoryUrls]
+  return [
+    ...pageUrls,
+    // ...postUrls,
+    ...categoryUrls,
+  ]
 }
