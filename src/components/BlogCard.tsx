7591c75cc8de9bd617dd752d653bbdb82dde859c
Neesh <avaneeshsamsi@gmail.com>
2023-06-11 16:04:29 +0530

    fix: Broken layout 2.0

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -41,13 +41,14 @@ const BlogCard: FC<BlogCardProps> = ({
         "flex flex-col gap-6 overflow-hidden rounded-xl bg-light",
         "shadow-md shadow-dark/5 transition-all duration-300 hover:-translate-y-1.5 hover:shadow-lg", // card hover
         {
+          "h-full": type === "Regular",
           "gap-4 xl:grid xl:grid-cols-1-2": type === "Sidebar",
         }
       )}
     >
       <div
-        className={cn("relative aspect-[4/3] h-full w-full", {
-          "xl:aspect-square": type === "Sidebar",
+        className={cn("relative aspect-[4/3] w-full", {
+          "h-full xl:aspect-square": type === "Sidebar",
         })}
       >
         <Image
