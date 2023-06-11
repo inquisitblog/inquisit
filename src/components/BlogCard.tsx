b496483badde0154aaf48772ffb24694c5039cab
Neesh <avaneeshsamsi@gmail.com>
2023-06-11 15:57:51 +0530

    fix: Broken layout

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -39,7 +39,7 @@ const BlogCard: FC<BlogCardProps> = ({
     <div
       className={cn(
         "flex flex-col gap-6 overflow-hidden rounded-xl bg-light",
-        "h-full shadow-md shadow-dark/5 transition-all duration-300 hover:-translate-y-1.5 hover:shadow-lg", // card hover
+        "shadow-md shadow-dark/5 transition-all duration-300 hover:-translate-y-1.5 hover:shadow-lg", // card hover
         {
           "gap-4 xl:grid xl:grid-cols-1-2": type === "Sidebar",
         }
