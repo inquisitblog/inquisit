14167b0f8b74c03889d60ea3c70707c2d48dc553
Neesh <avaneeshsamsi@gmail.com>
2023-06-25 12:21:27 +0530

    TS Sins - Eventually to be fixed by Keystatic

--- src/components/mdx/H2.tsx
+++ src/components/mdx/H2.tsx
@@ -1,8 +1,11 @@
-import type { FC, HTMLAttributes } from "react"
+import type { Component, FC, HTMLAttributes } from "react"
 import { CopyLinkID } from "./CopyLinkID"
 import { cn } from "@/lib/utils"
 
-const H2 = ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => {
+const H2: FC<HTMLAttributes<HTMLHeadingElement>> = ({
+  className,
+  ...props
+}) => {
   return (
     <div className="not-prose mb-6 mt-12 flex items-center gap-3 text-2xl font-bold text-dark/80 md:mb-8 md:mt-14 md:text-3xl/10">
       <h2 className={cn("", className)} {...props} />
