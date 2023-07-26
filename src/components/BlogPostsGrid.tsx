4b02da8ea6d38a025b33bf82b7058d9b2ca5b4ac
Neesh <avaneeshsamsi@gmail.com>
2023-07-26 15:55:49 +0530

    feat: Remove main, correct props

--- src/components/BlogPostsGrid.tsx
+++ src/components/BlogPostsGrid.tsx
@@ -6,31 +6,27 @@ type Props = {
 }
 
 const BlogPostsGrid: FC<Props> = ({ posts }) => {
-  return (
-    <main>
-      {posts.length ? (
-        <div className="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 lg:max-w-none lg:grid-cols-2">
-          {posts.map((post, i) => (
-            <BlogCard
-              key={post.slug}
-              type="Regular"
-              img={post.imgUrl}
-              alt={post.imgAlt}
-              date={post.date}
-              authors={post.authors}
-              title={post.title}
-              description={post.description}
-              tags={post.tags}
-              slug={post.slug}
-              // Prio image loading for the first 4 posts bcoz they will be above the fold
-              priority={i < 4}
-            />
-          ))}
-        </div>
-      ) : (
-        <p className="text-xl">No articles to show.</p>
-      )}
-    </main>
+  return posts.length ? (
+    <div className="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 lg:max-w-none lg:grid-cols-2">
+      {posts.map((post, i) => (
+        <BlogCard
+          key={post.slug}
+          type="Regular"
+          img={post.image}
+          alt={post.imageAlt}
+          date={post.pubDate}
+          authors={post.authors}
+          title={post.title}
+          description={post.description}
+          tags={post.categories}
+          slug={post.slug}
+          // Prio image loading for the first 4 posts bcoz they will be above the fold
+          priority={i < 4}
+        />
+      ))}
+    </div>
+  ) : (
+    <p className="text-xl">No articles to show.</p>
   )
 }
 
