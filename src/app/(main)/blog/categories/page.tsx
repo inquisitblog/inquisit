535b36650d3cb20e7b2edbc32f3d4f6836737457
Neesh <avaneeshsamsi@gmail.com>
2023-07-26 18:20:17 +0530

    feat: Use keystatic on blog categories page

--- src/app/(main)/blog/categories/page.tsx
+++ src/app/(main)/blog/categories/page.tsx
@@ -1,52 +1,63 @@
-import * as config from "@/lib/config"
-import { getPosts } from "@/lib/data"
-import { capitalise } from "@/lib/utils"
 import Link from "next/link"
+
+import reader from "@/lib/keystatic"
+import { getCategories, getPosts } from "@/lib/data"
+import { capitalise } from "@/lib/utils"
+
+import pluralize from "pluralize"
 import { ArrowRightIcon } from "@heroicons/react/24/outline"
-import { allCategories } from "contentlayer/generated"
 
-const title = "Blog Categories"
-const description = "All categories of blog posts."
+export async function generateMetadata() {
+  const blogcategoriespage = await reader.singletons.blogcategoriespage.read()
+  if (!blogcategoriespage)
+    throw new Error("Keystatic Content Not Found - Blog Categories Page")
 
-export const metadata = {
-  title,
-  description,
-  openGraph: {
+  const { metaTitle: title, metaDescription: description } = blogcategoriespage
+
+  return {
     title,
     description,
-    url: "/blog/categories",
-  },
-  twitter: {
-    title,
-    description,
-  },
-  alternates: {
-    canonical: "/blog/categories",
-    types: {
-      "application/rss+xml": `${config.url}/rss.xml`,
+    openGraph: {
+      title,
+      description,
+      url: "/blog/categories",
     },
-  },
+    twitter: {
+      title,
+      description,
+    },
+    alternates: {
+      canonical: "/blog/categories",
+    },
+  }
 }
 
 export default async function Category() {
-  const categories = allCategories
+  const blogcategoriespage = await reader.singletons.blogcategoriespage.read()
+  if (!blogcategoriespage)
+    throw new Error("Keystatic Content Not Found - Blog Categories Page")
 
-  const tags = await Promise.all(
-    categories.map(async (category) => ({
-      name: category.name,
-      slug: category.slug,
-      posts: await getPosts(-1, category.slug),
-    }))
+  const { headline, ctaText } = blogcategoriespage
+
+  const categories = await getCategories()
+
+  const categoriesWithPosts = await Promise.all(
+    categories.map(async ({ name, slug }) => ({
+      name,
+      slug,
+      posts: await getPosts({ number: -1, category: slug }),
+    })),
   )
 
   return (
-    <main className="mx-auto max-w-screen-2xl px-8 py-8 md:px-16 md:py-16">
+    <main className="mx-auto min-h-[80vh] max-w-screen-2xl px-8 py-8 md:px-16 md:py-16">
       <h1 className="mb-12 text-4xl font-bold text-accent md:mb-16 md:text-5xl xl:text-6xl">
-        Categories
+        {headline}
       </h1>
-      {tags.length ? (
+
+      {categoriesWithPosts.length ? (
         <div className="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 lg:max-w-none lg:grid-cols-2">
-          {tags.map(({ name, slug, posts }, i) => (
+          {categoriesWithPosts.map(({ name, slug, posts }, i) => (
             <div
               className="flex flex-col gap-2 rounded-xl bg-light p-4 md:gap-4 md:p-8"
               key={i}
@@ -54,12 +65,14 @@ export default async function Category() {
               <h2 className="text-2xl font-semibold leading-7 xl:text-3xl">
                 {capitalise(name)}
               </h2>
-              <p className="text-lg font-medium xl:text-xl">{`${posts.length} posts`}</p>
+              <p className="text-lg font-medium xl:text-xl">
+                {pluralize("post", posts.length, true)}
+              </p>
               <Link
                 href={`/blog/categories/${slug}`}
                 className="flex w-fit items-center gap-2 rounded border-2 border-accent px-2 py-1 text-base font-semibold text-accent transition-all hover:bg-accent hover:text-light xl:text-lg"
               >
-                <span>Check them out</span>
+                <span>{ctaText}</span>
                 <span className="w-5">
                   <ArrowRightIcon strokeWidth={2} />
                 </span>
