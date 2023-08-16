2b4f57123fc6db1dc8458672ea8329cce851c0d7
Neesh <avaneeshsamsi@gmail.com>
2023-08-16 12:09:27 +0530

    feat: Error - custom image patgs

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -344,25 +344,25 @@ export default config({
           links: true,
           dividers: true,
           // componentBlocks,
-          images: true,
-          // images: {
-          //   directory: "/public/images/blog-images/",
-          //   publicPath: "/images/blog-images/",
-          //   schema: {
-          //     alt: fields.text({
-          //       label: "Image Alternate Text",
-          //       description:
-          //         "This is read out to visually impaired users and displayed in a situation where the image was unable to load for any reason.",
-          //       validation: {
-          //         length: { min: 1 },
-          //       },
-          //     }),
-          //     title: fields.text({
-          //       label: "Image Caption",
-          //       description: "This is optional. Displayed under the image.",
-          //     }),
-          //   },
-          // },
+          // images: true,
+          images: {
+            directory: "public/images/blogposts/",
+            publicPath: "/images/blogposts/",
+            // schema: {
+            //   alt: fields.text({
+            //     label: "Image Alternate Text",
+            //     description:
+            //       "This is read out to visually impaired users and displayed in a situation where the image was unable to load for any reason.",
+            //     validation: {
+            //       length: { min: 1 },
+            //     },
+            //   }),
+            //   title: fields.text({
+            //     label: "Image Caption",
+            //     description: "This is optional. Displayed under the image.",
+            //   }),
+            // },
+          },
         }),
       },
     }),
