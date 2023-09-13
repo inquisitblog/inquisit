ca287ae324a6d0f6ed801724a25171727037507f
Neesh <avaneeshsamsi@gmail.com>
2023-09-13 10:16:20 +0530

    fix: Alignment with Navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,25 +15,23 @@ type Props = {
 const Footer = ({ siteName, footerTagline, email, navLinks }: Props) => {
   return (
     <footer className="bg-light py-8">
-      <div className="mx-auto max-w-screen-2xl px-8 text-lg">
-        <div className="flex flex-col justify-between gap-12 text-center md:flex-row md:gap-0 md:text-start">
-          <div className="flex flex-col gap-2">
-            <p className="text-xl font-[600]">{siteName}</p>
-            <p>{footerTagline}</p>
-          </div>
+      <div className="mx-auto flex max-w-screen-2xl flex-col justify-between gap-12 px-8 text-center text-lg md:flex-row md:gap-0 md:px-16 md:text-start">
+        <div className="flex flex-col gap-2">
+          <p className="text-xl font-[600]">{siteName}</p>
+          <p>{footerTagline}</p>
+        </div>
 
-          <div className="flex flex-col gap-2">
-            <p className="font-[500]">Reach out</p>
-            <a
-              href={`mailto:${email}`}
-              className="underline-offset-4 transition-all hover:underline"
-            >
-              {email}
-            </a>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <NavLinks navLinks={navLinks} />
-          </div>
+        <div className="flex flex-col gap-2">
+          <p className="font-[500]">Reach out</p>
+          <a
+            href={`mailto:${email}`}
+            className="underline-offset-4 transition-all hover:underline"
+          >
+            {email}
+          </a>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <NavLinks navLinks={navLinks} />
         </div>
       </div>
     </footer>
