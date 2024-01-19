5fa16fbcec647d8f9dd755861dc9c1cfb87a38f0
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-01-19 22:23:16 +0530

    feat: Dynamic post number & styling

--- src/components/PostCollection.tsx
+++ src/components/PostCollection.tsx
@@ -1,40 +1,63 @@
 import Link from "next/link"
+import { ArrowRightIcon } from "@heroicons/react/24/outline"
+import { cn } from "@/lib/utils"
 import BlogAuthors from "./BlogAuthors"
 import PostCard from "./PostCard"
-import { ArrowRightIcon } from "@heroicons/react/24/outline"
 
 type PostCollectionProps = {
   collection: PostCollection
+  type: "single" | "multiple"
+  postLimit?: number | 0
 }
 
-const PostCollection = ({ collection }: PostCollectionProps) => {
+const PostCollection = ({
+  collection,
+  type,
+  postLimit,
+}: PostCollectionProps) => {
   let { posts } = collection
-  posts = posts.slice(0, 2)
+
+  if (postLimit !== 0 || -1) {
+    posts = posts.slice(0, postLimit)
+  }
 
   return (
     <div className="grid gap-6">
       <Link
         href={`/collections/${collection.slug}`}
-        className="group grid w-fit gap-1"
+        className={cn("group grid w-fit", {
+          "gap-1": type === "multiple",
+          "gap-4": type === "single",
+        })}
       >
-        <h3 className="relative w-fit text-2xl font-semibold before:absolute before:-bottom-px before:left-0  before:h-[2px] before:w-full before:scale-x-0 before:bg-accent  before:transition-transform group-hover:before:scale-x-100 xl:text-3xl">
-          {collection.title}
-        </h3>
+        {type === "single" ? (
+          <h1 className="text-4xl font-bold text-accent xl:text-6xl">
+            {collection.title}
+          </h1>
+        ) : (
+          <h3 className="relative w-fit text-2xl font-semibold before:absolute before:-bottom-px before:left-0 before:h-[2px] before:w-full before:scale-x-0 before:bg-accent  before:transition-transform group-hover:before:scale-x-100 xl:text-3xl">
+            {collection.title}
+          </h3>
+        )}
         <BlogAuthors authors={collection.authors} />
         <p className="max-w-[60ch] text-xl text-dark/70">
           {collection.description}
         </p>
       </Link>
+
       <PostsStack posts={posts} />
-      <Link href={`/collections/${collection.slug}`}>
-        <button className="group flex w-fit items-center gap-2 rounded border-2 border-dark px-3 py-2 text-lg font-medium text-dark transition-all hover:bg-dark hover:text-light">
-          <span>{collection.buttonText || "Read complete collection"}</span>
-          <ArrowRightIcon
-            className="aspect-square w-6 transition-transform group-hover:translate-x-1"
-            strokeWidth={2}
-          />
-        </button>
-      </Link>
+
+      {type === "multiple" && (
+        <Link href={`/collections/${collection.slug}`}>
+          <button className="group flex w-fit items-center gap-2 rounded border-2 border-dark px-3 py-2 text-lg font-medium text-dark transition-all hover:bg-dark hover:text-light">
+            <span>{collection.buttonText || "Read complete collection"}</span>
+            <ArrowRightIcon
+              className="aspect-square w-6 transition-transform group-hover:translate-x-1"
+              strokeWidth={2}
+            />
+          </button>
+        </Link>
+      )}
     </div>
   )
 }
