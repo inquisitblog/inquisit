0475d42191be7c52690c2fa34c98556d5519f893
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-01-20 20:27:12 +0530

    fix: New categories path

--- src/components/BlogTags.tsx
+++ src/components/BlogTags.tsx
@@ -12,7 +12,7 @@ const BlogTags: FC<Props> = ({ tags }) => {
         <Link
           key={i}
           className="w-fit rounded border border-accent px-2 py-1 text-base transition-all hover:bg-accent hover:text-light 2xl:text-lg"
-          href={`/blog/categories/${tag.slug}`}
+          href={`/categories/${tag.slug}`}
         >
           {tag.name}
         </Link>
