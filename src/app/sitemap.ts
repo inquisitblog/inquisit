c4c99301ec24cfd82619f5f73c059599856959d7
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-02-01 16:30:16 +0530

    feat: Add pages from v1 updates

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,11 +7,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const settings = await reader.singletons.settings.read()
   if (!settings) throw new Error("Keystatic Content Not Found - Site Settings.")
 
+  const collections = await reader.collections.collections.list()
   const categories = await reader.collections.categories.list()
+  const authors = await reader.collections.authors.list()
 
   const { url } = settings
 
-  const pages = ["", "blog", "categories"]
+  const pages = ["", "blog", "collections", "categories", "authors"]
   const pageUrls = pages.map((page) => ({
     url: `${url}/${page}`,
     lastModified: new Date(),
@@ -20,14 +22,30 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getPosts()
 
   const postUrls = posts.map((post) => ({
-    url: `${url}/blog/${post.slug}`,
+    url: `${url}/posts/${post.slug}`,
     lastModified: new Date(post.updatedDate),
   }))
 
-  const categoryUrls = categories.map((category) => ({
-    url: `${url}/blog/categories/${category}`,
+  const collectionUrls = collections.map((collection) => ({
+    url: `${url}/collections/${collection}`,
     lastModified: new Date(),
   }))
 
-  return [...pageUrls, ...postUrls, ...categoryUrls]
+  const categoryUrls = categories.map((category) => ({
+    url: `${url}/categories/${category}`,
+    lastModified: new Date(),
+  }))
+
+  const authorUrls = authors.map((author) => ({
+    url: `${url}/authors/${author}`,
+    lastModified: new Date(),
+  }))
+
+  return [
+    ...pageUrls,
+    ...postUrls,
+    ...collectionUrls,
+    ...categoryUrls,
+    ...authorUrls,
+  ]
 }
